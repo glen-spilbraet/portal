bd9e1e565a5a52b6d8e35cee0de355fd4b9bab3f
Glen Randelovic Michaelsen <spilbraet-glen@AirtilhendeGlen.localdomain>
2026-04-25 23:12:04 +0200

    Configure Cloudflare adapter, wrangler, and dependencies

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,16 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
-// for information about these interfaces
 declare global {
 	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
+		interface Platform {
+			env: {
+				DB: import('@cloudflare/workers-types').D1Database;
+				IMAGES: import('@cloudflare/workers-types').R2Bucket;
+				PHOTOS: import('@cloudflare/workers-types').R2Bucket;
+				APP_PASSWORD: string;
+				APP_SECRET: string;
+				RACKBEAT_API_KEY?: string;
+			};
+		}
 	}
 }
 
